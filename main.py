c22cbf93bc40b7d99749a770dcf3dbd08d12f9b0
ahsanaah29-hub <ahsanaah29@gmail.com>
2026-09-15 12:31:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -586,7 +586,7 @@ async def sync_delivery_channels():
         res = supabase.table("orders") \
             .select("id, shopify_order_id, order_number, delivery_channel") \
             .eq("delivery_channel", "Pending") \
-            .limit(50) \
+            .eq("order_number", "215586")\
             .execute()
 
         print("PENDING ORDERS FOUND:", len(res.data))
